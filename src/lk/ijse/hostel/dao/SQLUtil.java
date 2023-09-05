1a304b7d64888fd19039cb91d59829fb78c27001
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-09-05 19:07:43 +0530

    SQLUtil

--- src/lk/ijse/hostel/dao/SQLUtil.java
+++ src/lk/ijse/hostel/dao/SQLUtil.java
@@ -1,8 +1,6 @@
 package lk.ijse.hostel.dao;
 
 public interface SQLUtil<T> extends SuperDAO{
-    boolean save(T entity);
-    boolean delete(T entity);
-    boolean update(T entity);
-//    ArrayList<T> get();
+
 }
+
