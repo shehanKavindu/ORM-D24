de240cf09e330960bb7159774bed7de3c8f32b66
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-09-05 19:10:34 +0530

    update SQLUtil

--- src/lk/ijse/hostel/dao/SQLUtil.java
+++ src/lk/ijse/hostel/dao/SQLUtil.java
@@ -1,6 +1,11 @@
 package lk.ijse.hostel.dao;
 
-public interface SQLUtil<T> extends SuperDAO{
+import java.util.ArrayList;
 
+public interface SQLUtil<T> extends SuperDAO{
+boolean save(T entity);
+boolean delete(T entity);
+Boolean update(T entity);
+//ArrayList<T> get();//
 }
 
